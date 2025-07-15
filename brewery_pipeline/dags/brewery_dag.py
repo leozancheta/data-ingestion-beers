ca5b3de6278172de1d9f56672a30caa863dba34f
leozancheta <leozancheta@gmail.com>
2025-07-14 23:55:17 -0300

    Remove unused files and restructure the brewery pipeline with new extraction, transformation, and aggregation scripts

--- brewery_pipeline/dags/brewery_dag.py
+++ brewery_pipeline/dags/brewery_dag.py
@@ -1,31 +1,43 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
-from src.extract import fetch_and_store_raw_data
-from src.transform import transform_to_silver, aggregate_to_gold
+import sys
+sys.path.append('/opt/airflow/scripts')
+from scripts.extract import extract_data
+from scripts.transform import transform_data
+from scripts.aggregate import aggregate_data
 
-with DAG(
+default_args = {
+    'owner': 'airflow',
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG(
     'brewery_pipeline',
-    start_date=datetime(2024, 1, 1),
+    default_args=default_args,
+    description='Brewery Medallion Pipeline',
     schedule_interval='@daily',
+    start_date=datetime(2024, 1, 1),
     catchup=False,
-    retries=1,
-    retry_delay=timedelta(minutes=5),
-) as dag:
+)
 
-    fetch_raw = PythonOperator(
-        task_id='fetch_raw_data',
-        python_callable=fetch_and_store_raw_data,
-    )
+t1 = PythonOperator(
+    task_id='extract_data',
+    python_callable=extract_data,
+    dag=dag
+)
 
-    silver = PythonOperator(
-        task_id='transform_to_silver',
-        python_callable=transform_to_silver,
-    )
+t2 = PythonOperator(
+    task_id='transform_data',
+    python_callable=transform_data,
+    dag=dag
+)
 
-    gold = PythonOperator(
-        task_id='aggregate_to_gold',
-        python_callable=aggregate_to_gold,
-    )
+t3 = PythonOperator(
+    task_id='aggregate_data',
+    python_callable=aggregate_data,
+    dag=dag
+)
 
-    fetch_raw >> silver >> gold
+t1 >> t2 >> t3
